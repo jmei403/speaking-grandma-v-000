758d65ffe8930c1d2b3ac37ae69186b201ebb9ae
Jason Mei <jason.mei403@gmail.com>
2018-05-18 21:32:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
   if string == string.upcase && string == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIM!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     "NO, NOT SINCE 1938!"
   else
